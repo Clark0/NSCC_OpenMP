b8b6db12b4a98231cf681e1ce5b1f09607564eae
Clark <caoliu98@yahoo.com>
2018-10-28 10:53:56 +0800

    count_thread

--- count_thread.c
+++ count_thread.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 void main() {
-int count = 0;
-#pragma omp parallel
-{
-#pragma omp critical
-    count += 1;
-}
-    printf("Total number of threads %d\n", count);
+    int count = 0;
+    #pragma omp parallel
+    {
+        #pragma omp critical
+        count += 1;
+    }
+    int num = omp_get_thread_num();
+    if (num == 0)
+        printf("Total number of threads %d\n", count);
 }
